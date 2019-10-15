501a75ccabbc680f8f836b8975032332cef28120
AndraIonescu <andradenis.ionescu@gmail.com>
2019-10-15 10:31:07 +0200

    small adjustment in read_data file

--- read_data_movies.py
+++ read_data_movies.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 data_imdb = pd.read_csv('data/google/movies1/csv_files/imdb.csv', index_col=False)
 data_rt = pd.read_csv('data/google/movies1/csv_files/rotten_tomatoes.csv', index_col=False)
-# data_imdb.set_index('Unnamed: 0', inplace=True)
-# data_rt.set_index('Unnamed: 0', inplace=True)
+data_imdb.set_index('Unnamed: 0', inplace=True)
+data_rt.set_index('Unnamed: 0', inplace=True)
 data_imdb = data_imdb.fillna('')
 data_rt = data_rt.fillna('')
