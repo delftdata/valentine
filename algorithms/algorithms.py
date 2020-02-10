19939ac94ee9abb3a76880051b255a2f7340b3b2
kPsarakis <kpsarakis94@gmail.com>
2020-02-10 11:22:17 +0100

    integrate jaccard levenshtein to the framework

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -1,3 +1,4 @@
 from algorithms.clustering_scale.correlation_clustering_scale import *
 from algorithms.similarity_flooding.similarity_flooding import *
 from algorithms.cupid.cupid_model import *
+from algorithms.jaccard_levenshtein.jaccard_leven import *
--- algorithms/jaccard_levenshtein/jaccard_leven.py
+++ algorithms/jaccard_levenshtein/jaccard_leven.py
@@ -1,41 +1,18 @@
 from algorithms.base_matcher import BaseMatcher
 from data_loader.instance_loader import InstanceLoader
-import Levenshtein as lv
+import Levenshtein as Lv
 
 
 class JaccardLevenMatcher(BaseMatcher):
-
-    '''
-
+    """
     Class containing the methods for implementing a simple baseline matcher that uses Jaccard Similarity between
     columns to assess their correspondence score, enhanced by Levenshtein Distance.
-
-    '''
+    """
 
     def __init__(self, threshold_leven: float = 0.75):
         self.threshold_leven = threshold_leven
 
-    def jaccard_leven(self, list1, list2, threshold):
-
-        if len(set(list1)) < len(set(list2)):
-            set1 = set(list1)
-            set2 = set(list2)
-        else:
-            set1 = set(list2)
-            set2 = set(list1)
-
-        intersection_cnt = 0
-
-        for s1 in set1:
-            for s2 in set2:
-                if lv.ratio(str(s1), str(s2)) >= threshold:
-                    intersection_cnt += 1
-                    break
-
-        union_cnt = len(set1.union(set2))
-        return float(intersection_cnt) / union_cnt
-
-    def get_matches(self, source: InstanceLoader, target: InstanceLoader):
+    def get_matches(self, source: InstanceLoader, target: InstanceLoader, dataset_name: str):
 
         matches = dict()
         src_table = source.table.name
@@ -55,6 +32,23 @@ class JaccardLevenMatcher(BaseMatcher):
 
         return sorted_matches
 
+    @staticmethod
+    def jaccard_leven(list1, list2, threshold):
 
+        if len(set(list1)) < len(set(list2)):
+            set1 = set(list1)
+            set2 = set(list2)
+        else:
+            set1 = set(list2)
+            set2 = set(list1)
 
+        intersection_cnt = 0
 
+        for s1 in set1:
+            for s2 in set2:
+                if Lv.ratio(str(s1), str(s2)) >= threshold:
+                    intersection_cnt += 1
+                    break
+
+        union_cnt = len(set1.union(set2))
+        return float(intersection_cnt) / union_cnt
