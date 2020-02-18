1f014c53429dd1b444fbfeb4fbe78d01e339ddf2
kPsarakis <kpsarakis94@gmail.com>
2020-02-18 14:56:47 +0100

    update slurm scripts

--- run_slurm_jobs.sh
+++ run_slurm_jobs.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
 
-for folder in configuration_files/*
+algorithm_name=$1
+
+for folder in configuration_files/"$algorithm_name"/*
 do
-  	sbatch ./run_tasks.sh "$folder" &
+  if [ "$algorithm_name" = 'SimilarityFlooding' ] || [ "$algorithm_name" = 'Cupid' ]
+	then
+	  for config in "$folder"/*
+	  do
+	    sbatch ./run_simple_task.sh "$config"
+    done
+	elif [ "$algorithm_name" = 'Coma' ]
+	then
+	  for config in "$folder"/*
+	  do
+	    batch ./run_memory_intensive_task.sh "$config"
+    done
+	elif [ "$algorithm_name" = 'CorrelationClustering' ] || [ "$algorithm_name" = 'JaccardLevenMatcher' ]
+	then
+	  for config in "$folder"/*
+	  do
+	    sbatch ./run_parallel_task.sh "$config"
+    done
+	fi
 done
-wait
