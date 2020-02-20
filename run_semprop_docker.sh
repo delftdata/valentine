da4b3d1762e83c747dad6932604ddc33c271ec75
Asterios Katsifodimos <asteriosk@gmail.com>
2020-02-20 22:35:58 +0100

    with container wai

--- run_semprop_docker.sh
+++ run_semprop_docker.sh
@@ -2,16 +2,18 @@
 
 set -x
 
-# temp_file=$(mktemp /tmp/semprop-docker.XXXXXX) || { echo "Failed to create temp file"; exit 1; }
+temp_file=$(mktemp semprop-docker.XXXXXX) || { echo "Failed to create temp file"; exit 1; }
 
-docker run -a STDIN -a STDOUT -a STDERR -v `pwd`:/code/  \
+rm -f $temp_file
+
+docker run -v `pwd`:/code/  \
 		-w /code/ \
-#		--cidfile $temp_file		
-		--entrypoint python  \
-		-e "PYTHONPATH=/code/"	\
+		--cidfile=$temp_file \
+		--entrypoint=python  \
+		--env="PYTHONPATH=/code/" \
 		asteriosk/sempropenv:3.8.5 \
 		run_job.py -c $1
 
-# docker container wait `cat $temp_file`
+docker container wait `cat $temp_file`
 
 rm -f $temp_file
