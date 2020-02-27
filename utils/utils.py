1d79e601d9aaf9c427cd1a913539fee17dbff4ef
kPsarakis <kpsarakis94@gmail.com>
2020-02-27 21:09:48 +0100

    document the utils

--- utils/utils.py
+++ utils/utils.py
@@ -3,9 +3,22 @@ import math
 import os
 
 
-def is_sorted(matches: dict):
+def is_sorted(dictionary: dict):
+    """
+    Function that checks if a dict is sorted (true if sorted false if not)
+
+    Parameters
+    ----------
+    dictionary: dict
+        The dict to check
+
+    Returns
+    -------
+    bool
+        If the dict is sorted or not
+    """
     prev = None
-    for value in matches.values():
+    for value in dictionary.values():
         if prev is None:
             prev = value
         else:
@@ -14,27 +27,59 @@ def is_sorted(matches: dict):
     return True
 
 
-def convert_data_type(string: str):
+def convert_data_type(value: str):
+    """
+    Takes a value as a string and return it with its proper datatype attached
+
+    Parameters
+    ----------
+    value: str
+        The value that we want to find its datatype as a string
+
+    Returns
+    -------
+    The value with the proper datatype
+    """
     try:
-        f = float(string)
+        f = float(value)
         if f.is_integer():
             return int(f)
         return f
     except ValueError:
-        return string
+        return value
 
 
 def get_project_root():
+    """ Return the root of the project as a string """
     return str(Path(__file__).parent.parent)
 
 
 def create_folder(path: str):
+    """ Create a folder on the given path """
     if not os.path.exists(path):
         os.makedirs(path)
 
 
 def one_to_one_matches(matches: dict):
-    if len(matches) < 2:
+    """
+    A filter that takes a dict of column matches and returns a dict of 1 to 1 matches. The filter works in the following
+    way: At first it gets the median similarity of the set of the values and removes all matches
+    that have a similarity lower than that. Then from what remained it matches columns for me highest similarity
+    to the lowest till the columns have at most one match.
+
+    Parameters
+    ----------
+    matches : dict
+        The ranked list of matches
+
+    Returns
+    -------
+    dict
+        The ranked list of matches after the 1 to 1 filter
+    """
+    set_match_values = set(matches.values())
+
+    if len(set_match_values) < 2:
         return matches
 
     matched = dict()
@@ -42,11 +87,6 @@ def one_to_one_matches(matches: dict):
     for key in matches.keys():
         matched[key[0]] = False
         matched[key[1]] = False
-
-    set_match_values = set(matches.values())
-
-    if len(set_match_values) < 2:
-        return matches
 
     median = list(set_match_values)[math.ceil(len(set_match_values)/2)]
 
@@ -64,5 +104,6 @@ def one_to_one_matches(matches: dict):
     return matches1to1
 
 
-def get_table_from_dataset_path(ds_path):
+def get_table_from_dataset_path(ds_path: str):
+    """ Returns the table name from the dataset path """
     return ds_path.split(".")[0].split("/")[-1]
