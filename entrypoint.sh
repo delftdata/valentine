24bc7f6d946132e2bd2e4cf999bc2641a3f7c953
kPsarakis <kpsarakis94@gmail.com>
2020-02-27 21:09:08 +0100

    refactor bash scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,12 +14,12 @@ DATA="$SOURCE""data"
 
 #Copy the contents of this folder to TARGET
 
-mkdir -p $TARGET
-rsync -av --progress $SOURCE $TARGET --exclude data --exclude output
+mkdir -p ${TARGET}
+rsync -av --progress ${SOURCE} ${TARGET} --exclude data --exclude output
 
-cd $TARGET
+cd ${TARGET}
 
-ln -s $OUTPUT output
-ln -s $DATA data
+ln -s ${OUTPUT} output
+ln -s ${DATA} data
 
 python run_job.py -c "$1"
--- run_experiments.sh
+++ run_experiments.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
+if [[ -z "$1" ]]; then
   echo "Give me an algorithm name."
   exit 1
 fi
 
-if [ -z "$2" ]; then
+if [[ -z "$2" ]]; then
   echo "How many jobs should I run?"
   exit 1
 fi
@@ -13,17 +13,17 @@ fi
 algorithm_name=$1
 num_jobs=$2
 
-if [ "$algorithm_name" = 'SimilarityFlooding' ] || [ "$algorithm_name" = 'CorrelationClustering' ] || \
-[ "$algorithm_name" = 'JaccardLevenMatcher' ] || [ "$algorithm_name" = 'Coma' ]
+if [[ "$algorithm_name" = 'SimilarityFlooding' ]] || [[ "$algorithm_name" = 'CorrelationClustering' ]] || \
+[[ "$algorithm_name" = 'JaccardLevenMatcher' ]] || [[ "$algorithm_name" = 'Coma' ]]
 then
   parallel --jobs "$num_jobs" python run_job.py -c ::: configuration_files/"$algorithm_name"/*/*
-elif [ "$algorithm_name" = 'Cupid' ]
+elif [[ "$algorithm_name" = 'Cupid' ]]
 then
   for folder in configuration_files/"$algorithm_name"/*
   do
     parallel --jobs "$num_jobs" python run_job.py -c ::: "$folder"/*
   done
-elif [ "$algorithm_name" = 'SemProp' ]
+elif [[ "$algorithm_name" = 'SemProp' ]]
 then
   parallel --jobs "$num_jobs" ./run_semprop_docker.sh ::: configuration_files/"$algorithm_name"/*/*
 else
