c33ca4923f22b569b8c0af0123e045ce2093d1e4
kPsarakis <kpsarakis94@gmail.com>
2020-02-27 21:09:59 +0100

    document the metrics module

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -5,6 +5,24 @@ from utils.utils import one_to_one_matches
 
 
 def get_tp_fn(matches: dict, golden_standard: GoldenStandardLoader, n: int = None):
+    """
+    Calculate the true positive  and false negative numbers of the given matches
+
+    Parameters
+    ----------
+    matches : dict
+        Ranked list of matches from the match with higher similarity to lower
+    golden_standard : GoldenStandardLoader
+        An object that contains all the information about the golden standard
+    n : int, optional
+        The percentage number that we want to consider from the ranked list (matches)
+        e.g. (90) for 90% of the matches
+
+    Returns
+    -------
+    (int, int)
+        True positive and false negative counts
+    """
     tp = 0
     fn = 0
 
@@ -22,6 +40,24 @@ def get_tp_fn(matches: dict, golden_standard: GoldenStandardLoader, n: int = Non
 
 
 def get_fp(matches: dict, golden_standard: GoldenStandardLoader, n: int = None):
+    """
+    Calculate the false positive number of the given matches
+
+    Parameters
+    ----------
+    matches : dict
+        Ranked list of matches from the match with higher similarity to lower
+    golden_standard : GoldenStandardLoader
+        An object that contains all the information about the golden standard
+    n : int, optional
+        The percentage number that we want to consider from the ranked list (matches)
+        e.g. (90) for 90% of the matches
+
+    Returns
+    -------
+    int
+        False positive
+    """
     fp = 0
 
     all_matches = list(map(lambda m: frozenset(m), list(matches.keys())))
@@ -36,6 +72,24 @@ def get_fp(matches: dict, golden_standard: GoldenStandardLoader, n: int = None):
 
 
 def recall(matches: dict, golden_standard: GoldenStandardLoader, one_to_one=False):
+    """
+    Function that calculates the recall of the matches against the golden standard. If one_to_one is set to true, it
+    also performs an 1-1 match filer. Meaning that each column will match only with another one.
+
+    Parameters
+    ----------
+    matches : dict
+        Ranked list of matches from the match with higher similarity to lower
+    golden_standard : GoldenStandardLoader
+        An object that contains all the information about the golden standard
+    one_to_one : bool, optional
+        If to perform the 1-1 match filter
+
+    Returns
+    -------
+    float
+        The recall
+    """
     if one_to_one:
         matches = one_to_one_matches(matches)
     tp, fn = get_tp_fn(matches, golden_standard)
@@ -45,6 +99,24 @@ def recall(matches: dict, golden_standard: GoldenStandardLoader, one_to_one=Fals
 
 
 def precision(matches: dict, golden_standard: GoldenStandardLoader, one_to_one=False):
+    """
+    Function that calculates the precision of the matches against the golden standard. If one_to_one is set to true, it
+    also performs an 1-1 match filer. Meaning that each column will match only with another one.
+
+    Parameters
+    ----------
+    matches : dict
+        Ranked list of matches from the match with higher similarity to lower
+    golden_standard : GoldenStandardLoader
+        An object that contains all the information about the golden standard
+    one_to_one : bool, optional
+        If to perform the 1-1 match filter
+
+    Returns
+    -------
+    float
+        The precision
+    """
     if one_to_one:
         matches = one_to_one_matches(matches)
     tp, fn = get_tp_fn(matches, golden_standard)
@@ -55,6 +127,24 @@ def precision(matches: dict, golden_standard: GoldenStandardLoader, one_to_one=F
 
 
 def f1_score(matches: dict, golden_standard: GoldenStandardLoader, one_to_one=False):
+    """
+    Function that calculates the F1 score of the matches against the golden standard. If one_to_one is set to true, it
+    also performs an 1-1 match filer. Meaning that each column will match only with another one.
+
+    Parameters
+    ----------
+    matches : dict
+        Ranked list of matches from the match with higher similarity to lower
+    golden_standard : GoldenStandardLoader
+        An object that contains all the information about the golden standard
+    one_to_one : bool, optional
+        If to perform the 1-1 match filter
+
+    Returns
+    -------
+    float
+        The f1_score
+    """
     pr = precision(matches, golden_standard, one_to_one)
     re = recall(matches, golden_standard, one_to_one)
     if pr + re == 0:
@@ -63,6 +153,24 @@ def f1_score(matches: dict, golden_standard: GoldenStandardLoader, one_to_one=Fa
 
 
 def precision_at_n_percent(matches: dict, golden_standard: GoldenStandardLoader, n: int):
+    """
+    Function that calculates the precision at n %
+    e.g. if n is 10 then only the first 10% of the matches will be considered for the precision calculation
+
+    Parameters
+    ----------
+    matches : dict
+        Ranked list of matches from the match with higher similarity to lower
+    golden_standard : GoldenStandardLoader
+        An object that contains all the information about the golden standard
+    n : int
+        The integer percentage number
+
+    Returns
+    -------
+    float
+        The precision at n %
+    """
     number_to_keep = int(math.ceil((n / 100) * len(matches.keys())))
     tp, fn = get_tp_fn(matches, golden_standard, number_to_keep)
     fp = get_fp(matches, golden_standard, number_to_keep)
@@ -72,6 +180,23 @@ def precision_at_n_percent(matches: dict, golden_standard: GoldenStandardLoader,
 
 
 def recall_at_sizeof_ground_truth(matches: dict, golden_standard: GoldenStandardLoader):
+    """
+    Function that calculates the recall at the size of the ground truth.
+    e.g. if the size of ground truth size is 10 then only the first 10 matches will be considered for
+    the recall calculation
+
+    Parameters
+    ----------
+    matches : dict
+        Ranked list of matches from the match with higher similarity to lower
+    golden_standard : GoldenStandardLoader
+        An object that contains all the information about the golden standard
+
+    Returns
+    -------
+    float
+        The recall at the size of ground truth
+    """
     tp, fn = get_tp_fn(matches, golden_standard, golden_standard.size)
     if tp + fn == 0:
         return 0
