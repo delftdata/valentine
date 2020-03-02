36550320ef0cf2d8612da5c50087691b86fb09cc
kPsarakis <kpsarakis94@gmail.com>
2020-03-02 23:34:34 +0100

    stop GNU parallel's nagging

--- run_experiments.sh
+++ run_experiments.sh
@@ -16,16 +16,16 @@ num_jobs=$2
 if [[ "$algorithm_name" = 'SimilarityFlooding' ]] || [[ "$algorithm_name" = 'CorrelationClustering' ]] || \
 [[ "$algorithm_name" = 'JaccardLevenMatcher' ]] || [[ "$algorithm_name" = 'Coma' ]]
 then
-  parallel --jobs "$num_jobs" python run_job.py -c ::: configuration_files/"$algorithm_name"/*/*
+  parallel --will-cite --jobs "$num_jobs" python run_job.py -c ::: configuration_files/"$algorithm_name"/*/*
 elif [[ "$algorithm_name" = 'Cupid' ]]
 then
   for folder in configuration_files/"$algorithm_name"/*
   do
-    parallel --jobs "$num_jobs" python run_job.py -c ::: "$folder"/*
+    parallel --will-cite --jobs "$num_jobs" python run_job.py -c ::: "$folder"/*
   done
 elif [[ "$algorithm_name" = 'SemProp' ]]
 then
-  parallel --jobs "$num_jobs" ./run_semprop_docker.sh ::: configuration_files/"$algorithm_name"/*/*
+  parallel --will-cite --jobs "$num_jobs" ./run_semprop_docker.sh ::: configuration_files/"$algorithm_name"/*/*
 else
   echo "Not a valid algorithm name."
   exit 1
