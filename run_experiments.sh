70702f3daf2efea8aa5493b12e6f9f54fcf5ba2a
kPsarakis <kpsarakis94@gmail.com>
2020-06-25 02:08:55 +0200

    add EmbDI and stdout

--- run_experiments.sh
+++ run_experiments.sh
@@ -14,14 +14,14 @@ algorithm_name=$1
 num_jobs=$2
 
 if [[ "$algorithm_name" = 'SimilarityFlooding' ]] || [[ "$algorithm_name" = 'CorrelationClustering' ]] || \
-[[ "$algorithm_name" = 'JaccardLevenMatcher' ]] || [[ "$algorithm_name" = 'Coma' ]]
+[[ "$algorithm_name" = 'JaccardLevenMatcher' ]] || [[ "$algorithm_name" = 'Coma' ]] || [[ "$algorithm_name" = 'EmbDI' ]]
 then
-  parallel --will-cite --jobs "$num_jobs" python run_job.py -c ::: configuration_files/"$algorithm_name"/*/*
+  parallel --will-cite  --jobs "$num_jobs" -u python run_job.py -c ::: configuration_files/"$algorithm_name"/*/*
 elif [[ "$algorithm_name" = 'Cupid' ]]
 then
   for folder in configuration_files/"$algorithm_name"/*
   do
-    parallel --will-cite --jobs "$num_jobs" python run_job.py -c ::: "$folder"/*
+    parallel --will-cite --jobs "$num_jobs" -u python run_job.py -c ::: "$folder"/*
   done
 elif [[ "$algorithm_name" = 'SemProp' ]]
 then
