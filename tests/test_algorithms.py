1f790cbb5410a20bc0718fd831371a1c3f081b37
kPsarakis <kpsarakis94@gmail.com>
2021-09-30 02:01:09 +0200

    setup automated tests

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,24 +1,27 @@
 import unittest
+import os
 
 from valentine import Coma
 from valentine.data_sources.local_fs.local_fs_table import LocalFSTable
+
+script_dir = os.path.dirname(__file__)
 
 
 class TestAlgorithms(unittest.TestCase):
 
     def test_coma(self):
         # Load data
-        d1 = LocalFSTable('data/tiny_authors_1_joinable_vertical_co30_target.csv', load_data=True)
-        d2 = LocalFSTable('data/tiny_authors_1_joinable_vertical_co30_source.csv', load_data=True)
+        d1 = LocalFSTable(os.path.join(script_dir, 'data/tiny_authors_1_joinable_vertical_co30_target.csv'),
+                          load_data=True)
+        d2 = LocalFSTable(os.path.join(script_dir, 'data/tiny_authors_1_joinable_vertical_co30_source.csv'),
+                          load_data=True)
         # Test the schema variant of coma
         coma_matcher_schema = Coma(strategy="COMA_OPT")
         matches_coma_matcher_schema = coma_matcher_schema.get_matches(d1, d2)
-        print(matches_coma_matcher_schema)
         assert len(matches_coma_matcher_schema) > 0  # Check that it actually produced output
         # Test the instance variant of coma
         coma_matcher_instances = Coma(strategy="COMA_OPT_INST")
         matches_coma_matcher_instances = coma_matcher_instances.get_matches(d1, d2)
-        print(matches_coma_matcher_instances)
         assert len(matches_coma_matcher_instances) > 0  # Check that it actually produced output
         # Assume the Schema and instance should provide different results
         assert matches_coma_matcher_schema != matches_coma_matcher_instances
