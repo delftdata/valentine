c90ca214f77baffe65472b0020acae4133d96112
kPsarakis <kpsarakis94@gmail.com>
2021-10-03 15:04:16 +0200

    update example

--- examples/valentine_example.py
+++ examples/valentine_example.py
@@ -4,23 +4,28 @@ from valentine import valentine_match, valentine_metrics
 from valentine.algorithms import Coma
 
 
-# Load data using pandas
-d1_path = os.path.join('data', 'tiny_authors_1_joinable_vertical_co30_target.csv')
-d2_path = os.path.join('data', 'tiny_authors_1_joinable_vertical_co30_source.csv')
-df1 = pd.read_csv(d1_path)
-df2 = pd.read_csv(d2_path)
+def main():
+    # Load data using pandas
+    d1_path = os.path.join('data', 'tiny_authors_1_joinable_vertical_co30_target.csv')
+    d2_path = os.path.join('data', 'tiny_authors_1_joinable_vertical_co30_source.csv')
+    df1 = pd.read_csv(d1_path)
+    df2 = pd.read_csv(d2_path)
 
-# Instantiate matcher and run
-matcher = Coma(strategy="COMA_OPT")
-matches = valentine_match(df1, df2, matcher)
+    # Instantiate matcher and run
+    matcher = Coma(strategy="COMA_OPT")
+    matches = valentine_match(df1, df2, matcher)
 
-print(matches)
+    print(matches)
 
-# If golden standard available valentine could calculate the metrics
-golden_standard = [('Cited by', 'Cited by'),
-                   ('Authors', 'Authors'),
-                   ('EID', 'EID')]
+    # If golden standard available valentine could calculate the metrics
+    golden_standard = [('Cited by', 'Cited by'),
+                       ('Authors', 'Authors'),
+                       ('EID', 'EID')]
 
-metrics = valentine_metrics.all_metrics(matches, golden_standard)
+    metrics = valentine_metrics.all_metrics(matches, golden_standard)
 
-print(metrics)
+    print(metrics)
+
+
+if __name__ == '__main__':
+    main()
