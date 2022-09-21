fcdfae35dd5830cb982c1b077f6c164a6feabba2
kPsarakis <kpsarakis94@gmail.com>
2022-09-21 16:13:41 +0200

    remove 3.11 builds for now, pyemd does not support it yet

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
     ],
     keywords=['matching', 'valentine', 'schema matching', 'dataset discovery', 'coma', 'cupid', 'similarity flooding'],
     include_package_data=True,
-    python_requires='>=3.8,<3.12',
+    python_requires='>=3.8,<3.11',
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
