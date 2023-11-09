96430e7d897bdfdf3939deb58f97e3531774d372
kPsarakis <kpsarakis94@gmail.com>
2023-11-09 12:17:23 +0100

    bump version for new release

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ classifiers = [
 
 setuptools.setup(
     name='valentine',
-    version='0.1.8',
+    version='0.1.9',
     description='Valentine Matcher',
     classifiers=classifiers,
     license_files=('LICENSE',),
@@ -26,7 +26,7 @@ setuptools.setup(
     maintainer='Delft Data',
     maintainer_email='delftdatasystems@gmail.com',
     url='https://delftdata.github.io/valentine/',
-    download_url='https://github.com/delftdata/valentine/archive/refs/tags/v0.1.8.tar.gz',
+    download_url='https://github.com/delftdata/valentine/archive/refs/tags/v0.1.9.tar.gz',
     packages=setuptools.find_packages(exclude=('tests*', 'examples*')),
     install_requires=[
         'setuptools',
