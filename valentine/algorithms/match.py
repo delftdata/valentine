d3913d85f9e18dcd8328884c0f4cb1614d43826d
Shaad Alaka <shaad1@live.nl>
2024-01-24 16:14:11 +0100

    Refactor Match class to a dataclass

--- valentine/algorithms/match.py
+++ valentine/algorithms/match.py
@@ -1,20 +1,24 @@
-class Match(object):
-    """
-    Class representing a match of two columns target is the one we want to find the matches of, source an other
-    that exists in the database and the similarity between the two.
+from dataclasses import dataclass
+from typing import Dict, Tuple, Self
 
-    NOTE: Use the to_dict method when you want to append a match to a list of matches
+
+@dataclass
+class Match:
     """
-    def __init__(self, target_table_name: str, target_column_name: str,
-                 source_table_name: str, source_column_name: str,
-                 similarity: float):
-        self.target_table_name = target_table_name
-        self.target_column_name = target_column_name
-        self.source_table_name = source_table_name
-        self.source_column_name = source_column_name
-        self.similarity = similarity
+    Class representing a match of two columns. target is the one we want to
+    find the matches of, source an other that exists in the database and the
+    similarity between the two.
+
+    NOTE: Use the to_dict method when you want to append a match to a list of
+    matches
+    """
+    target_table_name: str
+    target_column_name: str
+    source_table_name: str
+    source_column_name: str
+    similarity: float
 
     @property
-    def to_dict(self) -> dict:
+    def to_dict(self: Self) -> Dict[Tuple[Tuple[str, str], Tuple[str, str]], float]:
         return {((self.source_table_name, self.source_column_name),
                  (self.target_table_name, self.target_column_name)): self.similarity}
